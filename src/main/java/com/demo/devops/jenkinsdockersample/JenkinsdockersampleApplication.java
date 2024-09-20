7fc6530c79871e7797dcb89e7735e20dc69023bd
dmnglore1 <124757036+dmnglore@users.noreply.github.com>
2024-09-20 09:55:32 +0530

    Update JenkinsdockersampleApplication.java

--- src/main/java/com/demo/devops/jenkinsdockersample/JenkinsdockersampleApplication.java
+++ src/main/java/com/demo/devops/jenkinsdockersample/JenkinsdockersampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsdockersampleApplication {
 
 	public static void main(String[] args) {
-System.out.println("Hello");
+System.out.println("Welcome to the world of devops");
 		SpringApplication.run(JenkinsdockersampleApplication.class, args);
 	}
 
